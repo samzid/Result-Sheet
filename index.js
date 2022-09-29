ed0583f3a2e678b61acec9c5505e85def6a2c499
SAMZID KHAN <65766247+samzid@users.noreply.github.com>
2022-09-29 17:04:56 +0600

    Result sheet update
    Result sheet

---
+++ index.js
@@ -0,0 +1,38 @@
+var grade ="Null"
+var point ="Null"
+function markC(totalMark)
+{
+    if(totalMark>=80 && totalMark<=100){
+        grade ="A+";
+        point="5.00";
+    }
+    else if (totalMark>=70 && totalMark <=79)
+    {
+        grade="A";
+        point="4.00";
+    }
+    else if (totalMark>=70 && totalMark <=79)
+    {
+        grade="A";
+        point="4.00";
+    }
+}
+else if (totalMark>=70 && totalMark <=79)
+{
+    grade="A";
+    point="4.00";
+}
+}
+else if (totalMark>=60 && totalMark <=69)
+{
+    grade="A-";
+    point="4.00";
+}
+}
+else if (totalMark>=70 && totalMark <=79)
+{
+    grade="A";
+    point="4.00";
+}
+}
+}
